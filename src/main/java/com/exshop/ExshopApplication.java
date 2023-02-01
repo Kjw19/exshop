4087b417712b3c985fec86e7bbca91a9c89f5836
김재우 <ksg3477@gmail.com>
2023-02-01 15:47:17 +0900

    Initial commit

---
+++ src/main/java/com/exshop/ExshopApplication.java
@@ -0,0 +1,15 @@
+package com.exshop;
+
+import com.exshop.config.SecurityConfig;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/exshop/ExshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
